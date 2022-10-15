ea740741fa632b3fd485ef99ecedf927127a3f93
Helena Paixao <helena.paixao@corteva.com>
2022-10-15 08:56:24 -0300

    fist commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import {Container, Title}from './styles'
+
+export default function Groups() {
+  return (
+        <Container>
+      <Title>Groups</Title>
+
+        </Container>
+  );
+}
+
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+flex: 1;
+background-color: #000;
+align-items: center;
+justify-content: center;
+
+`
+
+export const Title = styled.Text`
+color: #FFF;
+
+`;
