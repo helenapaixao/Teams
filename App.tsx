37da4136d8d49afe47d9cb04dca116e0fbf9514f
Helena Paixao <helena.paixao@corteva.com>
2022-10-15 09:23:26 -0300

    feat: statusbar stilized

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {ActivityIndicator} from 'react-native'
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
@@ -15,7 +15,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups/> : <Loading/>}
+      <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent={true}
+      />
+      {fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
