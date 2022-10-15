1ab2c7893e1e4adf32be86cf8154d91e545f5121
Helena Paixao <helena.paixao@corteva.com>
2022-10-15 09:19:53 -0300

    feat: add loading

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import {ActivityIndicator} from 'react-native'
 import { ThemeProvider } from "styled-components";
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import {Loading} from '@components/Loading'
 import theme from "./src/theme";
 
 import Groups from "@screens/Groups";
@@ -13,7 +15,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups/> : <ActivityIndicator/>}
+      {!fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+
+
+import { Container, LoadIndicator } from "./styles";
+
+
+export function Loading() {
+    return(
+        <Container>
+            <LoadIndicator />
+        </Container>
+    )
+}
