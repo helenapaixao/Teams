7bbb37df3644224e30920a7348ea2fa4977f0d2b
Helena Paixao <helena.paixao@corteva.com>
2022-10-24 08:43:10 -0300

    feat: add button secondary in players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,9 +7,9 @@ import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -45,17 +45,16 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
-
         ListEmptyComponent={() => (
           <ListEmpty message="Nenhum jogador encontrado" />
-          
         )}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={[
-          {paddingBottom: 100},
-          players.length === 0 && {flex: 1, justifyContent: 'center'}
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1, justifyContent: "center" },
         ]}
       />
+      <Button title="Remover turma" type="SECONDARY" onPress={() => {}} />
     </Container>
   );
 }
