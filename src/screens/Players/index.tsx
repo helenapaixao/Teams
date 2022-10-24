071821c48f3e2cfd993f4c968049b3642040aa9a
Helena Paixao <helena.paixao@corteva.com>
2022-10-24 08:41:10 -0300

    feat: listempty in center

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,13 +6,14 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-import { ListEmpty } from "@components/ListEmpty";
+
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Helena", "Teste"]);
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -44,10 +45,16 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+
         ListEmptyComponent={() => (
           <ListEmpty message="Nenhum jogador encontrado" />
           
         )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          {paddingBottom: 100},
+          players.length === 0 && {flex: 1, justifyContent: 'center'}
+        ]}
       />
     </Container>
   );
