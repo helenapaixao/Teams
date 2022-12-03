e8dde9ad4ea765124f9e2b9d9e979c53b6d0414e
Helena Paixao <helena.paixao@corteva.com>
2022-12-03 19:23:40 -0300

    feat: add navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
+
 
 export default function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
   const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate("new")
+    navigation.navigate("new");
   }
 
   return (
@@ -29,10 +31,7 @@ export default function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button
-      title="Criar nova turma"
-      onPress={handleNewGroup}
-      />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
