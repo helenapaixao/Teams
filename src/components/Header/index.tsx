be8d2d672a0ef4128bf19b5587fe39bc9818adc1
Helena Paixao <helena.paixao@corteva.com>
2022-12-03 20:25:50 -0300

    feat: add button back

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,25 @@
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: Props) {
+const navigation = useNavigation();
+
+
+  function  handleGoBack()  {
+   navigation.goBack();
+  };
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
-          <BackIcon />
+        <BackButton  onPress={handleGoBack}>
+          <BackIcon/>
         </BackButton>
       )}
       <Logo source={logoImg} />
