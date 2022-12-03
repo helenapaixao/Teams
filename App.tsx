38fbab0347ffa3cc34652aacca4e26193d4b34e8
Helena Paixao <helena.paixao@corteva.com>
2022-12-03 17:09:03 -0300

    feat: add react navigator

--- App.tsx
+++ App.tsx
@@ -1,28 +1,32 @@
-
 import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-
-import {Loading} from '@components/Loading'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { NavigationContainer } from "@react-navigation/native";
+import { Loading } from "@components/Loading";
 import theme from "./src/theme";
 
-import {Players} from "@screens/Players";
-
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent={true}
-      />
-      {fontsLoaded ? <Players/> : <Loading/>}
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent={true}
+        />
+        {fontsLoaded ? <Players /> : <Loading />}
+      </ThemeProvider>
+    </NavigationContainer>
   );
 }
