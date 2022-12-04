ad5b2a4deb7ed3046be1311fc5fe1f2e4cade741
Helena Paixao <helena.paixao@corteva.com>
2022-12-04 12:32:06 -0300

    fix: config glitch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
-import {NavigationContainer} from '@react-navigation/native'
-import {AppRoutes} from './app.routes'
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./app.routes";
+import { useTheme } from "styled-components/native";
+
 export function Routes() {
-    return (
-        <NavigationContainer independent={true}>
-            <AppRoutes />
-        </NavigationContainer>
-    
-    )
+  const { COLORS } = useTheme();
+  return (
+    <View style={{
+        flex: 1,
+        backgroundColor: COLORS.GRAY_600
+    }}>
+      <NavigationContainer independent={true}>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
+  );
 }
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -12,12 +12,18 @@ import { Button } from "@components/Button";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { useRoute } from "@react-navigation/native";
 
+
+type RouteParams = {
+  group: string;
+};
+
+
 export function Players() {
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([]);
 
   const route = useRoute();
-  const { group } = route.params;
+  const { group } = route.params as RouteParams;
 
   return (
     <Container>
